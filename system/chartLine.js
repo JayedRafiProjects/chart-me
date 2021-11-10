608657e59ee5472c59a7cd91ed7f93ef86fdf0da
Jayed Rafi <jayedrafi@outlook.com>
2021-11-10 12:27:01 -0600

    Update chartLine.js

--- system/chartLine.js
+++ system/chartLine.js
@@ -10,18 +10,18 @@
 *******************************/
 
 //Global variables
-var xAxis = []; //initially empty x axis
-var yAxis = []; //initially empty y axis
+var xAxisLine = []; //initially empty x axis
+var yAxisLine = []; //initially empty y axis
 
 /*******************************
 * Takes the values from user input
 * Assigns those values into xAxis and yAxis
 *******************************/
-function inputPoints() {
-  var xVal= document.getElementById('x-axis').value; //x axis
-  var yVal = document.getElementById('y-axis').value; //y axis
-  xAxis.push(xVal);
-  yAxis.push(yVal);
+function inputPointsLine() {
+  var xVal= document.getElementById('x-axis-line').value; //x axis
+  var yVal = document.getElementById('y-axis-line').value; //y axis
+  xAxisLine.push(xVal);
+  yAxisLine.push(yVal);
   return false;
 }
 
@@ -29,21 +29,24 @@ function inputPoints() {
 * Makes the cart visible
 * Draws the chart based on xAxis (label) & yAxis
 *******************************/
-function process(){
+function processLine(){
 new Chart("line-chart",{
   type: "line",
   data: {
-    labels: xAxis,
+    labels: xAxisLine,
     datasets: [{
       fill: false,
       lineTension: 0,
       borderColor: "rgba(86,101,115,1)", //gray
 		pointBackgroundColor:"rgba(245,84,39,0.8)",//red
-      data: yAxis
+      data: yAxisLine
     }]
   },
   options: {
     legend: {display: false},
+	scales:{
+		  beginAtZero: true
+	  }
   }
 });
 }
@@ -52,8 +55,8 @@ new Chart("line-chart",{
 * Makes both xAxis and yAxis empty
 * Visualize the empty chart
 *******************************/
-function refresh(){
-	xAxis.length=0;
-	yAxis.length=0;
-	process();
+function refreshLine(){
+	xAxisLine.length=0;
+	yAxisLine.length=0;
+	processLine();
 }
