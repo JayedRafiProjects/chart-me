bbbee9c6bf0e123e2a664551600bee10e8a2cc16
Jayed Rafi <jayedrafi@outlook.com>
2021-11-10 23:41:05 -0600

    Update Chart.js

--- system/Chart.js
+++ system/Chart.js
@@ -1,9 +1,4 @@
-/*!
- * Chart.js v2.9.4
- * https://www.chartjs.org
- * (c) 2020 Chart.js Contributors
- * Released under the MIT License
- */
+
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(function() { try { return require('moment'); } catch(e) { } }()) :
 typeof define === 'function' && define.amd ? define(['require'], function(require) { return factory(function() { try { return require('moment'); } catch(e) { } }()); }) :
